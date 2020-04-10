83a62a57268daf48f3e010df97982a2284c2fb9f
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-10 22:50:04 +0300

    Added "I Am Interested In" section to student page.

--- src/routes/StudentHome.svelte
+++ src/routes/StudentHome.svelte
@@ -1,5 +1,8 @@
 <script>
 	import StudentMyCurriculum from './StudentMyCurriculum.svelte';
+	import StudentIAmInterestedIn from './StudentIAmInterestedIn.svelte';
 </script>
 
 <StudentMyCurriculum/>
+
+<StudentIAmInterestedIn/>
---
+++ src/routes/StudentIAmInterestedIn.svelte
@@ -0,0 +1,23 @@
+<h2 class="section-title">I Am Interested In</h2>
+<table>
+    <thead>
+        <tr>
+            <td>Subjects</td>
+            <td>University Offers</td>
+            <td>Employer Needs</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Subject 2</td>
+            <td>no</td>
+            <td>30</td>
+        </tr>
+        <tr>
+            <td>Subject 3</td>
+            <td>no</td>
+            <td>no</td>
+        </tr>
+    </tbody>
+</table>
+<button>Add a Subject</button>
