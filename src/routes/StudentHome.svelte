54e8d92347c81a156f08756404d005154c831beb
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-10 23:34:18 +0300

    Added "If i decided to choose" section to student profile.

--- src/routes/StudentHome.svelte
+++ src/routes/StudentHome.svelte
@@ -2,6 +2,7 @@
 	import StudentMyCurriculum from './StudentMyCurriculum.svelte';
 	import StudentIAmInterestedIn from './StudentIAmInterestedIn.svelte';
 	import StudentMatchingEmployers from './StudentMatchingEmployers.svelte';
+	import StudentIfIDecidedToChoose from './StudentIfIDecidedToChoose.svelte';
 </script>
 
 <StudentMyCurriculum/>
@@ -9,3 +10,5 @@
 <StudentIAmInterestedIn/>
 
 <StudentMatchingEmployers/>
+
+<StudentIfIDecidedToChoose/>:
---
+++ src/routes/StudentIfIDecidedToChoose.svelte
@@ -0,0 +1,23 @@
+<script>
+	import SectionTitle from '../components/SectionTitle.svelte';
+</script>
+
+<SectionTitle sectionTitle="If I Decided to Choose"/>
+<input type="checkbox" value="subject6" />Subject 6
+<input type="checkbox" value="subject2" />Subject 2
+<ul>
+    <li>
+        <div class="employer-with-match">
+            <img src="default-employer-logo.png" alt="Company 6 Logo" />
+            <span>Company 6</span>
+            <span>90%</span>
+        </div>
+    </li>
+    <li>
+        <div class="employer-with-match">
+            <img src="default-employer-logo.png" alt="Company 7 Logo" />
+            <span>Company 7</span>
+            <span>75%</span>
+        </div>
+    </li>
+</ul>
