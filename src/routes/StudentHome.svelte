9e9d90edcfb375aef4ad19909518dc9b96e0eb93
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-10 23:15:35 +0300

    Added "Matching Employers" section to student profile.

--- src/routes/StudentHome.svelte
+++ src/routes/StudentHome.svelte
@@ -1,8 +1,11 @@
 <script>
 	import StudentMyCurriculum from './StudentMyCurriculum.svelte';
 	import StudentIAmInterestedIn from './StudentIAmInterestedIn.svelte';
+	import StudentMatchingEmployers from './StudentMatchingEmployers.svelte';
 </script>
 
 <StudentMyCurriculum/>
 
 <StudentIAmInterestedIn/>
+
+<StudentMatchingEmployers/>
---
+++ src/routes/StudentMatchingEmployers.svelte
@@ -0,0 +1,34 @@
+<script>
+	import SectionTitle from '../components/SectionTitle.svelte';
+</script>
+
+<SectionTitle sectionTitle="Matching Employers"/>
+<select>
+    <option value="" selected="selected">-- All Fields --</option>
+    <option value="marketing">Marketing</option>
+    <option value="management">Management</option>
+    <option value="it">IT</option>
+</select>
+<ul>
+    <li>
+        <div class="employer-with-match">
+            <img src="default-employer-logo.png" alt="Company 1 Logo" />
+            <span>Company 1</span>
+            <span>100%</span>
+        </div>
+    </li>
+    <li>
+        <div class="employer-with-match">
+            <img src="default-employer-logo.png" alt="Company 2 Logo" />
+            <span>Company 2</span>
+            <span>80%</span>
+        </div>
+    </li>
+    <li>
+        <div class="employer-with-match">
+            <img src="default-employer-logo.png" alt="Company 3 Logo" />
+            <span>Company 3</span>
+            <span>67%</span>
+        </div>
+    </li>
+</ul>
