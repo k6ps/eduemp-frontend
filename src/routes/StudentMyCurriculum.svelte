459141337d5802769173970665e39a05baefe5b9
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-11 11:02:00 +0300

    Including subjects from filtered array instead of hard-coding.

--- src/routes/StudentMyCurriculum.svelte
+++ src/routes/StudentMyCurriculum.svelte
@@ -1,5 +1,19 @@
 <script>
-	import SectionTitle from '../components/SectionTitle.svelte';
+    import SectionTitle from '../components/SectionTitle.svelte';
+    
+    const allSubjects = [
+        {
+            id: "subject1",
+            title: "Subject 1",
+            employerNeeds: true
+        },
+        {
+            id: "subject3",
+            title: "Subject 333",
+            employerNeeds: false
+        },
+    ];
+    let mySubjectIds = ["subject1", "subject3"];
 </script>
 
 <SectionTitle sectionTitle="My Curriculum"/>
@@ -11,14 +25,14 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>Subject 1</td>
-            <td>yes</td>
-        </tr>
-        <tr>
-            <td>Subject 3</td>
-            <td>no</td>
-        </tr>
+        {#each allSubjects as subject}
+            {#if mySubjectIds.includes(subject.id)}
+                <tr>
+                    <td>{subject.title}</td>
+                    <td>{subject.employerNeeds ? 'yes' : 'no'}</td>
+                </tr>
+            {/if}
+        {/each}
     </tbody>
 </table>
 <a class="button">Add a Subject</a>
