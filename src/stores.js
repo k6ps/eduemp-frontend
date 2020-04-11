57dd9e9ddd5e90619e04aa46f5c5fbf75b5328f5
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-11 15:00:12 +0300

    Added students' interest data to subjects.

--- src/stores.js
+++ src/stores.js
@@ -5,49 +5,65 @@ export const allSubjectsData = readable([
 		id: "subject1",
 		title: "Anatomy and Physiology",
 		uniersityOffers: true,
-		employerNeeds: 10
+		employerNeeds: 10,
+		studentsInCurriculum: 80,
+		studentsInterested: 111
 	},
 	{
 		id: "subject2",
 		title: "Foundations of Nursing",
 		uniersityOffers: false,
-		employerNeeds: 17
+		employerNeeds: 17,
+		studentsInCurriculum: 443,
+		studentsInterested: 34
 	},
 	{
 		id: "subject3",
 		title: "Internal Patient Nursing",
 		uniersityOffers: false,
-		employerNeeds: 3
+		employerNeeds: 3,
+		studentsInCurriculum: 8,
+		studentsInterested: 8
 	},
 	{
 		id: "subject4",
 		title: "Mental Health Nursing",
 		uniersityOffers: true,
-		employerNeeds: 25
+		employerNeeds: 25,
+		studentsInCurriculum: 23,
+		studentsInterested: 344
 	},
 	{
 		id: "subject5",
 		title: "Psychology",
 		uniersityOffers: false,
-		employerNeeds: 20
+		employerNeeds: 20,
+		studentsInCurriculum: 223,
+		studentsInterested: 342
 	},
 	{
 		id: "subject6",
 		title: "War and Disaster Medicine",
 		uniersityOffers: false,
-		employerNeeds: 0
+		employerNeeds: 0,
+		studentsInCurriculum: 11,
+		studentsInterested: 212
 	},
 	{
 		id: "subject7",
 		title: "Adult Health Behaviour",
 		uniersityOffers: true,
-		employerNeeds: 0
+		employerNeeds: 0,
+		studentsInCurriculum: 23,
+		studentsInterested: 224
 	},
 	{
 		id: "subject8",
 		title: "In-Depth Knowledge in Intensive Nursing",
 		uniersityOffers: true,
-		employerNeeds: 2
+		employerNeeds: 2,
+		studentsInCurriculum: 10,
+		studentsInterested: 11
 	},
 ]);
 
