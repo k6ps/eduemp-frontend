d4d73f16998fa50f7e41594f66c99f035fa5204c
Taavi Tänavsuu <tanavst@gmail.com>
2020-11-08 14:10:52 +0200

    Created add e-mail to waitlist component in employer home page.

---
+++ src/components/AddEMailToWaitList.svelte
@@ -0,0 +1,12 @@
+<script>
+	let eMailAddress = '';
+
+    function openAddSubjectDialog() {
+        alert("Thank you, " + eMailAddress + ", for your interest!");
+    }
+</script>
+
+<div class="add-email-to-waiting-list">
+    <input bind:value={eMailAddress} placeholder="Please enter your e-mail" />
+    <a class="button" on:click="{addToInterestedList}">I am interested!</a>
+</div>
--- src/routes/EmployerHome.svelte
+++ src/routes/EmployerHome.svelte
@@ -1,9 +1,13 @@
 <script>
 	import EmployerMyRequirements from './EmployerMyRequirements.svelte';
 	import EmployerMatchingStudents from './EmployerMatchingStudents.svelte';
+	import AddEMailToWaitList from './AddEMailToWaitList.svelte';
 </script>
 
 <div class="content">
+    <div>
+        <AddEMailToWaitList/>
+    </div>
     <div class="wrapper wrapper-of-boxes">
         <div class="col col-50 col-box">
             <span>Testss</span>
