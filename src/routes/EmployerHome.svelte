ff7b59d6d47117525a878f1198a981ebb43b9bb3
Taavi Tänavsuu <tanavst@gmail.com>
2020-11-08 13:09:51 +0200

    A sample modification to test autodeploy

--- src/routes/EmployerHome.svelte
+++ src/routes/EmployerHome.svelte
@@ -4,8 +4,9 @@
 </script>
 
 <div class="content">
-		<div class="wrapper wrapper-of-boxes">
-				<div class="col col-50 col-box">
+    <div class="wrapper wrapper-of-boxes">
+        <div class="col col-50 col-box">
+            <span>Testss</span>
             <EmployerMyRequirements/>
         </div>
         <div class="col col-50 col-box">
