97b65f723f067390de0cc6f51b2668380ecadeb6
Taavi Tänavsuu <tanavst@gmail.com>
2020-11-08 14:56:17 +0200

    Added a role selection to wait list

--- src/components/AddEMailToWaitList.svelte
+++ src/components/AddEMailToWaitList.svelte
@@ -1,12 +1,28 @@
 <script>
-	let eMailAddress = '';
+    let eMailAddress = '';
+    let role = '';
 
     function addToInterestedList() {
-        alert("Thank you, " + eMailAddress + ", for your interest!");
+        alert("Thank you, " + eMailAddress + " (" + role + "), for your interest!");
+        eMailAddress = '';
+        role = '';
     }
 </script>
 
 <div class="add-email-to-waiting-list">
+    <span>I am a: </span>
+    <label>
+        <input type="radio" bind:group={role} value={'student'}>
+        Student
+    </label>
+    <label>
+        <input type="radio" bind:group={role} value={'employer'}>
+        Employer representative
+    </label>
+    <label>
+        <input type="radio" bind:group={role} value={'university'}>
+        University representative
+    </label>
     <input bind:value={eMailAddress} placeholder="Please enter your e-mail" />
     <a class="button" on:click="{addToInterestedList}">I am interested!</a>
 </div>
